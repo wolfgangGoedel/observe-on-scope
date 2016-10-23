9a87e20a722bb3899dabf593e2be634a621778d6
Wolfgang Goedel <wgoedel@gmail.com>
2016-10-23 13:15:36 +0200

    tests

--- src/observeOnScope.spec.ts
+++ src/observeOnScope.spec.ts
@@ -1,7 +1,7 @@
-import { IScope, IRootScopeService, ITimeoutService } from 'angular';
+import { IScope, IRootScopeService } from 'angular';
 import { expect } from 'chai';
-import { spy } from 'sinon';
-import { Observable, TestScheduler } from 'rxjs';
+import { spy, stub, SinonSpy } from 'sinon';
+import { Observable, Subject, Subscriber } from 'rxjs';
 import '../test/mocks';
 import './index';
 
@@ -15,32 +15,127 @@ function createScope() {
   return {$scope, $rootScope};
 }
 
+function provideExceptionHandler($provide: ng.auto.IProvideService) {
+  $provide.constant('$exceptionHandler', spy());
+}
+
 describe(`observeOnScope`, () => {
 
-  //mock.module.sharedInjector();
-  beforeEach(mock.module());
+  beforeEach(mock.module(provideExceptionHandler));
 
-  it('should work with empty', () => {
-    const {$scope} = createScope();
-    const obs = Observable.empty();
-    let completed = false;
-    obs.observeOnScope($scope).subscribe(
-      _ => { throw 'must not next'; },
-      _ => { throw 'must not error'; },
-      () => completed = true
-    );
-    expect(completed).to.be.true;
+  describe('applies correctly', () => {
+    it('should propagate next in scope.$apply() context', () => {
+      const {$scope} = createScope();
+      let phase: string;
+      Observable.never<boolean>().startWith(true)
+        .observeOnScope($scope)
+        .subscribe(
+          _ => phase = $scope.$$phase,
+          _ => {},
+          () => {}
+        );
+      expect(phase).to.equal('$apply');
+    });
+
+    it('should propagate error in scope.$apply() context', () => {
+      const {$scope} = createScope();
+      let phase: string;
+      Observable.of(true).map(_ => { throw "error" })
+        .observeOnScope($scope)
+        .subscribe(
+          _ => {},
+          _ => phase = $scope.$$phase,
+          () => {}
+        );
+      expect(phase).to.equal('$apply');
+    });
+
+    it('should propagate complete in scope.$apply() context', () => {
+      const {$scope} = createScope();
+      let phase: string;
+      Observable.empty()
+        .observeOnScope($scope)
+        .subscribe(
+          _ => {},
+          _ => {},
+          () => phase = $scope.$$phase
+        );
+      expect(phase).to.equal('$apply');
+    });
+
+    it('should not throw when already applied', () => {
+      const {$scope} = createScope();
+      const source = new Subject<void>();
+      let calledWithoutError = false;
+      source
+        .observeOnScope($scope)
+        .subscribe(
+          _ => calledWithoutError = true,
+          _ => { throw "must not error"; }
+        );
+      $scope.$apply(() => source.next());
+      expect(calledWithoutError).to.be.true;
+    });
+
+    it('should let $exceptionHandler catch errors not handled in subscribe', () => {
+      let $exceptionHandler: SinonSpy;
+      inject((_$exceptionHandler_: any) => $exceptionHandler = _$exceptionHandler_);
+      const {$scope} = createScope();
+      const source = new Subject<void>();
+      source
+        .observeOnScope($scope)
+        .subscribe();
+      source.error("some error");
+      expect($exceptionHandler.calledOnce).to.be.true;
+    });
+  });
+  
+  describe('lifecyle', () => {
+    it('should complete when scope is destroyed', () => {
+      const {$scope} = createScope();
+      const obs = Observable.never();
+      let completed = false;
+      obs.observeOnScope($scope).subscribe(null, null, () => completed = true);
+      
+      $scope.$destroy();
+      expect(completed).to.be.true;
+    });
+
+    it('should unsubscribe from source when scope in destroyed', () => {
+      const {$scope} = createScope();
+      let unsubscribed = false;
+      const source = new Observable<void>((subscriber: Subscriber<void>) => {
+        return () => unsubscribed = true;
+      });
+      source.observeOnScope($scope).subscribe();
+      
+      $scope.$destroy();
+      expect(unsubscribed).to.be.true;
+    });
+
+    it('should remove $onDestroy listener when it completes', () => {
+      const {$scope} = createScope();
+      let removed = false;
+      stub($scope, '$on').returns(() => removed = true);
+      Observable.empty()
+        .observeOnScope($scope)
+        .subscribe();
+      expect(removed).to.be.true;
+    });
+
+    it('should remove $onDestroy listener when it errors', () => {
+      const {$scope} = createScope();
+      let removed = false;
+      stub($scope, '$on').returns(() => removed = true);
+      const source = new Subject<void>();
+      source
+        .observeOnScope($scope)
+        .subscribe();
+      source.error('some error');
+      expect(removed).to.be.true;
+    });
   });
 
-  it('should complete when scope is destroyed', () => {
-    const {$scope} = createScope();
-    const obs = Observable.never();
-    let completed = false;
-    obs.observeOnScope($scope).subscribe(null, null, () => completed = true);
-    
-    $scope.$destroy();
-    expect(completed).to.be.true;
-  });
 });
 
 describe('$scope behaviour', () => {
@@ -51,21 +146,21 @@ describe('$scope behaviour', () => {
     expect(applied).to.be.true;
   });
 
-  it('should have its $$phase set when in a digest loop', () => {
+  it('should have its $$phase set to "$apply" in passed lambda to $apply() on itself', () => {
     const {$scope} = createScope();
     let phase: any;
     $scope.$apply(() => phase = $scope.$$phase);
-    expect(phase).not.to.be.undefined;
+    expect(phase).to.equal('$apply');
   });
 
-  it('should have its $$phase set when its root in a digest loop', () => {
+  it('should have its $$phase set to "$apply" in passed lambda to $apply() on root', () => {
     const {$scope, $rootScope} = createScope();
     let phase: any;
     $rootScope.$apply(() => phase = $scope.$$phase);
     expect(phase).not.to.be.undefined;
   });
 
-  it('should throw when calling $apply in a digest loop', () => {
+  it('should throw when calling $apply in context of an $apply() call', () => {
     const {$scope, $rootScope} = createScope();
     const shouldThrow = () => $scope.$apply();
     expect(() => $rootScope.$apply(shouldThrow)).to.throw();
